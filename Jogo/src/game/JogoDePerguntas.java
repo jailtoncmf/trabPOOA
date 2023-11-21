0f37030b297250d790d00e43fc9cb7305b2d2a27
Jailton Mocitaiba <jailton.mocitaiba@gmail.com>
2023-11-20 22:43:08 -0300

    c

--- Jogo/src/game/JogoDePerguntas.java
+++ Jogo/src/game/JogoDePerguntas.java
@@ -5,8 +5,8 @@ import java.util.Scanner;
 public class JogoDePerguntas {
 	
 	private static String[][] alternativas = {
-            {"0", "1", "2", "3"},
-            {"400", "450", "500", "550"},
+            {"Nenhuma", "Uma", "Duas", "Três"},
+            {"400 Gols", "450 Gols", "500 Gols", "550 Gols"},
             {"Barcelona", "Real Madrid", "Bayern de Munique", "Liverpool"}
     };
 	
@@ -23,26 +23,26 @@ public class JogoDePerguntas {
         
 
         String[] respostas = {
-        		"0",
-                "450",
+        		"Nenhuma",
+                "450 Gols",
                 "Real Madrid"
         };
 
         for (int i = 0; i < perguntas.length; i++) {
             System.out.println("Pergunta " + (i + 1) + ": " + perguntas[i]);
-            
-            for (int j = 0; j < alternativas[i].length; j++) {
-                System.out.println((char)('A' + j) + ". " + alternativas[i][j]);
-            }
-            
-            String resposta = scanner.nextLine();
-            
 
-            if (resposta.equalsIgnoreCase(respostas[i])) {
+            for (int j = 0; j < alternativas[i].length; j++) {
+                System.out.println((char) ('A' + j) + ") " + alternativas[i][j]);
+            }
+            System.out.print("Sua resposta: ");
+
+            String resposta = scanner.nextLine().toLowerCase(); 
+
+            if (resposta.equals(respostas[i].toLowerCase())) {
                 System.out.println("Resposta correta! Você ganhou 10 pontos.");
                 pontuacao += 10;
             } else {
-                System.out.println("Resposta incorreta. A resposta correta é: " + respostas[i]);
+                System.out.println("Resposta incorreta.");
             }
         }
 
@@ -54,4 +54,3 @@ public class JogoDePerguntas {
         }
     }
 }
-
--- Jogo/src/game/JogoDePerguntasGUI.java
+++ Jogo/src/game/JogoDePerguntasGUI.java
@@ -8,95 +8,134 @@ import java.awt.event.ActionListener;
 public class JogoDePerguntasGUI extends JFrame {
     private int pontuacao = 0;
     private int perguntaAtual = 0;
+
+    private String[][] alternativas = {
+            {"Nenhuma", "Uma", "Duas", "Três"},
+            {"400 Gols", "450 Gols", "500 Gols", "550 Gols"},
+            {"Barcelona", "Real Madrid", "Bayern de Munique", "Liverpool"}
+    };
+
     private String[] perguntas = {
             "Quantas Bola de Ouro tem o Neymar?",
             "Quantos Gols Cristiano Ronaldo fez pelo Real Madrid?",
             "Quem é o Clube com mais Champions League?"
     };
-    
-    private String[][] alternativas = {
-            {"0 \n", "1 \n", "2 \n", "3 \n"},
-            {"400", "450", "500", "550"},
-            {"Barcelona", "Real Madrid", "Bayern de Munique", "Liverpool"}
-    };
-    
+
     private String[] respostas = {
-            "0",
-            "450",
+            "Nenhuma",
+            "450 Gols",
             "Real Madrid"
     };
 
     private JFrame frame;
     private JPanel panel;
     private JLabel perguntaLabel;
-    private JTextField respostaField;
+    private JButton[] alternativaButtons;
     private JButton proximaPerguntaButton;
+    private JButton tentarNovamenteButton;
     private JLabel feedbackLabel;
     private JLabel pontuacaoLabel;
 
     public JogoDePerguntasGUI() {
         frame = new JFrame("Jogo de Perguntas e Respostas");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(400, 200);
+        frame.setSize(850, 300);
         frame.setLayout(new BorderLayout());
 
         panel = new JPanel();
-        panel.setLayout(new GridLayout(4, 1));
+        panel.setLayout(new GridLayout(5, 1));
 
         perguntaLabel = new JLabel();
-        respostaField = new JTextField();
+        alternativaButtons = new JButton[4];
+        for (int i = 0; i < alternativaButtons.length; i++) {
+            alternativaButtons[i] = new JButton();
+            alternativaButtons[i].addActionListener(new AlternativaButtonListener());
+            panel.add(alternativaButtons[i]);
+        }
         proximaPerguntaButton = new JButton("Próxima Pergunta");
+        proximaPerguntaButton.addActionListener(new ActionListener() {
+        	
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                exibirProximaPergunta();
+            }
+        });
+        
+        
         feedbackLabel = new JLabel();
         pontuacaoLabel = new JLabel();
 
-        proximaPerguntaButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                verificarResposta();
-            }
-        });
-
         panel.add(perguntaLabel);
-        panel.add(respostaField);
         panel.add(proximaPerguntaButton);
         panel.add(feedbackLabel);
 
         frame.add(panel, BorderLayout.CENTER);
         frame.add(pontuacaoLabel, BorderLayout.SOUTH);
+        
+
 
         exibirProximaPergunta();
 
         frame.setVisible(true);
+        
+        JButton tentarNovamenteButton = new JButton("Tentar Novamente");
+        
+        tentarNovamenteButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                reiniciarJogo();
+                feedbackLabel.setText("          JOGO REINICIADO");
+            }
+        });
+        panel.add(tentarNovamenteButton);
     }
+    
 
     private void exibirProximaPergunta() {
         if (perguntaAtual < perguntas.length) {
             perguntaLabel.setText("Pergunta " + (perguntaAtual + 1) + ": " + perguntas[perguntaAtual]);
-            respostaField.setText("");
-            feedbackLabel.setText("");
-            for (int i = 0; i < alternativas[perguntaAtual].length; i++) {
-                perguntaLabel.setText(perguntaLabel.getText() + "\n" + (char)('A' + i) + ". " + alternativas[perguntaAtual][i]);
+
+            for (int i = 0; i < alternativaButtons.length; i++) {
+                alternativaButtons[i].setText((char)('A' + i) + ". " + alternativas[perguntaAtual][i]);
             }
+
+            proximaPerguntaButton.setEnabled(false);
+            feedbackLabel.setText("");
         } else {
             perguntaLabel.setText("Jogo concluído!");
-            respostaField.setEnabled(false);
             proximaPerguntaButton.setEnabled(false);
             feedbackLabel.setText("Pontuação final: " + pontuacao);
         }
     }
 
-    private void verificarResposta() {
-        String resposta = respostaField.getText();
-        if (perguntaAtual < perguntas.length && resposta.equalsIgnoreCase(respostas[perguntaAtual])) {
-            feedbackLabel.setText("Resposta correta! Você ganhou 10 pontos.");
-            pontuacao += 10;
-        } else {
-            feedbackLabel.setText("Resposta incorreta. A resposta correta é: " + respostas[perguntaAtual]);
+    private class AlternativaButtonListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            JButton sourceButton = (JButton) e.getSource();
+            String respostaEscolhida = sourceButton.getText().substring(3); 
+            verificarResposta(respostaEscolhida);
         }
-        perguntaAtual++;
-        exibirProximaPergunta();
     }
 
+    private void verificarResposta(String respostaEscolhida) {
+        if (respostaEscolhida.equalsIgnoreCase(respostas[perguntaAtual])) {
+            feedbackLabel.setText("Resposta correta! Você ganhou 10 pontos.");
+            pontuacao += 10;
+            proximaPerguntaButton.setEnabled(true);
+        } else {
+        	feedbackLabel.setText("Resposta incorreta.");
+        	proximaPerguntaButton.setEnabled(false);
+            tentarNovamenteButton.setEnabled(true); 
+            
+        }
+
+        perguntaAtual++;
+    }
+    private void reiniciarJogo() {
+        perguntaAtual = 0;
+        pontuacao = 0;
+        exibirProximaPergunta();
+    }
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
             @Override
@@ -106,4 +145,3 @@ public class JogoDePerguntasGUI extends JFrame {
         });
     }
 }
-
